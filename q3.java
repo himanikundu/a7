3af46d45af14e28f6103fee55711cf701ec4ead2
Himani <himanikunndu026@gamil.com>
2018-08-28 21:41:30 +0530

    assignment 7

---
+++ q3.java
@@ -0,0 +1,16 @@
+final class A{  
+ void show(){
+ System.out.println("Class A");
+}                                       
+}            
+class B extends A{  
+ void show(){
+ System.out.println("Class B");
+}                                       
+}
+class q3{
+public static void main(String args[]){  
+B obj= new B(); 
+obj.show();
+}
+}
